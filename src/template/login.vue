36488c07979ea560d29f0891e5ad781d38b2c396
1694871021 <1694871021@qq.com>
2022-07-30 21:10:22 +0800

    原始版本提交

---
+++ src/template/login.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <span>文章序号:</span>
+    <input type="num" v-model="count" />
+  </div>
+</template>
+<script>
+export default {
+  name: "login",
+  data() {
+    return {
+      count: 1,
+    };
+  },
+  watch: {
+    count(val) {
+      this.$store.dispatch("increment1", val);
+    },
+  },
+  mounted() {
+    this.$store.dispatch("increment1", 1);
+  },
+  methods: {},
+};
+</script>
+<style scoped>
+span {
+  margin-left: 10px;
+}
+input {
+  margin: 10px;
+  height: 30px;
+  width: 200px;
+}
+</style>
---
+++ src/template/user.vue
@@ -0,0 +1,37 @@
+<template>
+  <div v-if="content.length">
+    <div class="cont" v-for="item in content" :key="item.id">
+      <div>{{ item.id }}. {{ item.title }}</div>
+      <div>{{ item.detailTitle }}</div>
+      <div class="example">{{ item.content }}</div>
+    </div>
+  </div>
+  <div class="cont" v-else>
+    <div>{{ content.id }}. {{ content.title }}</div>
+    <div>{{ content.detailTitle }}</div>
+    <div class="example">{{ content.content }}</div>
+  </div>
+</template>
+<script>
+export default {
+  name: "login",
+  data() {
+    return {};
+  },
+  computed: {
+    content() {
+      return this.$store.state.content;
+    },
+  },
+  methods: {},
+};
+</script>
+<style scoped>
+.cont > div {
+  margin: 10px;
+}
+.example {
+  color: red;
+  font-size: 15px;
+}
+</style>
